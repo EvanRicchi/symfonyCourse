980c5db0ead2801e7283910a7c2430652182d665
EvanRicchi <e.ricchi@hotmail.fr>
2018-03-13 23:15:24 +0100

    Update show

--- src/AppBundle/Controller/Api/ShowController.php
+++ src/AppBundle/Controller/Api/ShowController.php
@@ -8,6 +8,9 @@ use JMS\Serializer\SerializerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @Route(name="api_show_")
@@ -51,5 +54,24 @@ class ShowController extends Controller
         $this->getDoctrine()->getManager()->flush();
 
         return $this->returnResponse('Show has been deleted.', Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/shows/{id}", name="update")
+     * @Method("PUT")
+     */
+    public function putAction(Show $show, Request $request, SerializerInterface $serializer, ValidatorInterface $validator) {
+        $newShow = $serializer->deserialize($request->getContent(),Show::class,'json');
+        $constraintValidationList = $validator->validate($show);
+
+        if($constraintValidationList->count() == 0) {
+            $show->update($newShow);
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return new Response('Show has been updated.', Response::HTTP_OK);
+        } else {
+            return new Response($serializer->serialize($constraintValidationList, 'json'),Response::HTTP_BAD_REQUEST);
+        }
     }
 }
